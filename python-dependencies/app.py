3b4aa6d693819eb36ce07aaaf5f21bf51903ce4b
adrien-legros <alegros@redhat.com>
2023-02-14 11:35:59 +0100

    change python app

--- python-dependencies/app.py
+++ python-dependencies/app.py
@@ -1,49 +1,25 @@
-#!/usr/bin/python
-from flask import Flask, request, jsonify, make_response
-import requests
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-import time
+from flask import Flask, Response
+import json
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/api/loadtest/v1/healthz')
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/healthz")
 def healthz():
-    return make_response(jsonify({"health": "ok"}), 200)
+    return Response("{'status':'ok'}", status=200, mimetype='application/json')
 
-@application.route('/api/loadtest/v1/mem/<int:size>/<int:seconds>')
-def memory(size, seconds):
-    try:
-      dummy = ' ' * 1024 * 1024 * size
-    except MemoryError:
-      return make_response("Ran out of memory", 400)
-    time.sleep(seconds)
-    dummy = ''
+def generate_test_response():
+    response = {"fail":"false"}
+    return response
 
-    return make_response("", 200)
-
-def f(x):
-    loop_count = 1000 * 1024 * 250
-    while loop_count > 0:
-      x*x
-      loop_count = loop_count - 1
-
-@application.route('/api/loadtest/v1/cpu/<int:cpus>')
-def cpu(cpus):
-
-    print('Number of cpus available: %d' % cpu_count())
-    if (cpus > cpu_count()):
-        return make_response("Requested cpus > number of cores available.", 400)
-
-    print('-' * 20)
-    print('Running load on CPU(s)')
-    print('Utilizing %d cores' % cpus)
-    print('-' * 20)
-    pool = Pool(cpus)
-    pool.map(f, range(cpus))
-    pool.close()
-
-    return make_response("", 200)
+@app.route("/test")
+def test_app():
+    response = generate_test_response()
+    resp_json = json.dumps(response)
+    return Response(resp_json, status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-     application.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
